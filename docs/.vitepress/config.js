b47030acd769201640fee00e612bac0e66d468ff
Egor Gorev <apps@egorev.com>
2024-08-24 17:38:09 +0200

    Update config.js (Claude help)

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -14,13 +14,11 @@ export default {
         link: '/posts/',
         items: [
           { text: 'Article 1', link: '/posts/article-1' }
-          // Add more articles here as you create them
         ]
       }
     ],
     socialLinks: [
       { icon: 'instagram', link: 'https://instagram.com/egorev' }
-      // Add more social links here if needed
     ]
   }
 }
