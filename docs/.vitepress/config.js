290ba8f045b774eb2edf74e7e49cb58bb3e89315
Egor Gorev <apps@egorev.com>
2024-08-24 16:45:50 +0200

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+export default {
+  title: 'My VitePress Blog',
+  description: 'A simple blog created with VitePress',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Posts', link: '/posts/' },
+    ],
+    sidebar: [
+      {
+        text: 'Posts',
+        items: [
+          // You'll add your posts here later
+        ]
+      }
+    ]
+  }
+}
