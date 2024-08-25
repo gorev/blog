efd8293691f5668d7397336c95c6cc0c06d7c8b0
Egor Gorev <apps@egorev.com>
2024-08-25 13:05:17 +0200

    Update index.js (Adding Phoyoswipe plugin)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
+import PhotoSwipe from 'vitepress-plugin-photoswipe'
+import 'photoswipe/style.css'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.use(PhotoSwipe)
+  }
+}
