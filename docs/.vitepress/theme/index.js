4df0f2d285b286e694509c6cf7f1d96a3b136f49
Egor Gorev <apps@egorev.com>
2024-08-25 13:14:35 +0200

    Update index.js (reverted PhotoSwipe)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
-import PhotoSwipe from 'vitepress-plugin-photoswipe'
-import 'photoswipe/style.css'
 import './custom.css'
 
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.use(PhotoSwipe)
-  }
-}
+export default DefaultTheme
