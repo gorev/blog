1c0d660070cb448df8f87cbdbfaed925942308f6
Egor Gorev <apps@egorev.com>
2024-08-26 21:08:58 +0200

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,17 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-import mediumZoom from 'medium-zoom'
-import { onMounted } from 'vue'
 
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.mixin({
-      mounted() {
-        onMounted(() => {
-          mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' });
-        })
-      }
-    })
-  }
-}
+export default DefaultTheme
